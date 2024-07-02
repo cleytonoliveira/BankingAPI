f8283f97e541080d3abd0001640272d3b620b7f3
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2024-07-02 15:16:37 -0300

    test: endpoint GET `/balance` for existing account

--- tests/integration/balance.test.js
+++ tests/integration/balance.test.js
@@ -7,4 +7,21 @@ describe("GET to /balance", () => {
     expect(response.status).toBe(404);
     expect(response.body).toEqual(0);
   });
+
+  it("should return 200 and balance for existing account", async () => {
+    await request(app).post("/event").send({
+      type: "deposit",
+      destination: "100",
+      amount: 10,
+    });
+    await request(app).post("/event").send({
+      type: "deposit",
+      destination: "100",
+      amount: 10,
+    });
+
+    const response = await request(app).get("/balance?account_id=100");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(20);
+  });
 });
